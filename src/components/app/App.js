aa1ea6b16661c9fbafb1e9559a155ddbf42cd22c
dzmitry.staniuk <dzmitry.staniuk@leverx.com>
2021-10-09 16:23:47 +0300

    feat: base header implementation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Header from "../header";
 
 const App = () => {
-  return <div className={'main'}></div>;
+  return <div className={'main'}>
+    <Header />
+  </div>;
 }
 
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+import './header.css';
+
+const DEFAULT_CLASSNAME = 'header';
+
+const LOGO_TITLE = "VOTVETE";
+const LOGO_SUBTITLE = 'Аксессуары | Стёкла | Чехлы'
+
+
+
+const Logo = () => (
+  <div className={`${DEFAULT_CLASSNAME}_logo`}>
+    <img src={logo} alt={'logo'} />
+    <div className={`${DEFAULT_CLASSNAME}_logo-text`}>
+      <span className={`${DEFAULT_CLASSNAME}_logo-text-title`}>{LOGO_TITLE}</span>
+      <span className={`${DEFAULT_CLASSNAME}_logo-text-subtitle`}>{LOGO_SUBTITLE}</span>
+    </div>
+  </div>
+)
+
+const HeaderMenu = () => (
+  <div className={`${DEFAULT_CLASSNAME}_menu`}>
+    <div className={`${DEFAULT_CLASSNAME}_menu-item active-menu-item`}>{'Главная'}</div>
+    <div className={`${DEFAULT_CLASSNAME}_menu-item`}>{'Каталог'}</div>
+  </div>
+)
+
+const Header = () => {
+  return (
+    <div className={DEFAULT_CLASSNAME}>
+      <Logo />
+      <HeaderMenu />
+    </div>
+  )
+};
+
+export default Header;
